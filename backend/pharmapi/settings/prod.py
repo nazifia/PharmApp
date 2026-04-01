cf2768b880a0997da28619227d4ca1df89371b9c
Nazifia <nazzsankira@gmail.com>
2026-04-01 12:25:03 +0100

    feat: update settings and configuration files for production environment

--- backend/pharmapi/settings/prod.py
+++ backend/pharmapi/settings/prod.py
@@ -115,3 +115,8 @@ LOGGING = {
         },
     },
 }
+
+
+# Static files                                                                                                                                
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # folder where collectstatic dumps files  
