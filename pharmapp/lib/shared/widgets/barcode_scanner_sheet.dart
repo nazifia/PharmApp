4dcf56c43374d2a64ee66632bd94cfa608ca7c29
Nazifia <nazzsankira@gmail.com>
2026-05-18 21:20:33 +0100

    refactor: simplify barcode scanner by removing camera startup loading state

--- pharmapp/lib/shared/widgets/barcode_scanner_sheet.dart
+++ pharmapp/lib/shared/widgets/barcode_scanner_sheet.dart
@@ -14,30 +14,13 @@ class BarcodeScannerSheet extends StatefulWidget {
 }
 
 class _BarcodeScannerSheetState extends State<BarcodeScannerSheet> {
-  late final MobileScannerController _controller;
+  final MobileScannerController _controller = MobileScannerController(
+    detectionSpeed: DetectionSpeed.normal,
+    facing: CameraFacing.back,
+  );
   final TextEditingController _manualCtrl = TextEditingController();
   bool _scanned = false;
   bool _cameraFailed = false;
-  bool _cameraStarting = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = MobileScannerController(
-      detectionSpeed: DetectionSpeed.normal,
-      facing: CameraFacing.back,
-    );
-    _startCamera();
-  }
-
-  Future<void> _startCamera() async {
-    try {
-      await _controller.start();
-      if (mounted) setState(() => _cameraStarting = false);
-    } catch (e) {
-      if (mounted) setState(() { _cameraFailed = true; _cameraStarting = false; });
-    }
-  }
 
   @override
   void dispose() {
@@ -119,13 +102,7 @@ class _BarcodeScannerSheetState extends State<BarcodeScannerSheet> {
             Expanded(
               child: _cameraFailed
                   ? _manualEntryView()
-                  : _cameraStarting
-                      ? const Center(
-                          child: CircularProgressIndicator(
-                            color: EnhancedTheme.primaryTeal,
-                          ),
-                        )
-                      : Stack(children: [
+                  : Stack(children: [
                       ClipRRect(
                         borderRadius: BorderRadius.circular(16),
                         child: MobileScanner(
@@ -143,7 +120,7 @@ class _BarcodeScannerSheetState extends State<BarcodeScannerSheet> {
                     ]),
             ),
             // Always-visible manual entry on web when camera is alive
-            if (kIsWeb && !_cameraFailed && !_cameraStarting) _manualEntryBar(),
+            if (kIsWeb && !_cameraFailed) _manualEntryBar(),
             const SizedBox(height: 24),
           ]),
         ),
